407c830cc1347063bad1f5a62a050f739411ef88
antukbala <antukbala@gmail.com>
2021-11-28 00:37:29 +0600

    Basic Version Done

--- app/src/main/java/com/antukbala/weatherapp/MainActivity.java
+++ app/src/main/java/com/antukbala/weatherapp/MainActivity.java
@@ -3,12 +3,60 @@ package com.antukbala.weatherapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editTextCity);
+        textView = findViewById(R.id.textViewWeather);
+    }
+
+    public void getWeatherUpdate(View view) {
+        String apiKey = "0f93a4a49fa83e9b7c05219faf55a786";
+        String cityName = editText.getText().toString().trim();
+        String url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=0f93a4a49fa83e9b7c05219faf55a786";
+
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject object = response.getJSONObject("main");
+                    String temp = object.getString("temp");
+                    Double centigrade = Double.parseDouble(temp)-273.15;
+                    textView.setText(centigrade.toString().substring(0,4));
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        queue.add(request);
     }
 }
